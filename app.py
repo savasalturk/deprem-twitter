fb29de62adc5643cecf4c7929893fee7e392eecc
savasalturk <60508433+savasalturk@users.noreply.github.com>
2023-02-08 04:30:07 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import folium
 import json
 
 m = folium.Map([37.265763040011514, 36.610105733926865],zoom_start = 8)
+marker_cluster = MarkerCluster().add_to(m)
 f = open("data/v6.geojson","r",encoding="utf-8").read()
 js = json.loads(f)
 fields = ['1', 'sehir', 'ilce', 'mah', 'Adres', 'Apartman Adı', 'Sokak/Cadde/Bulvar', 'Dış Kapı/Blok', 'İç Kapı', 'Kat', 'isim', 'tel', 'kaynak', 'tel.1', 'adres_birlesik', 'konum', 'x', 'y']
-folium.GeoJson(data=js,popup=folium.GeoJsonPopup(fields=fields)).add_to(m)
+folium.GeoJson(data=js,popup=folium.GeoJsonPopup(fields=fields)).add_to(marker_cluster)
 m.save("index.html")
 
 
